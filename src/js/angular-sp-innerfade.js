ec923d973981b0530b7403743f2b25856c54c748
supa-creation <contact@supa-creation.com>
2015-09-16 11:50:50 +0200

    Change controller initialisation to directive

--- src/js/angular-sp-innerfade.js
+++ src/js/angular-sp-innerfade.js
@@ -14,41 +14,58 @@
 
     var module = angular.module('spInnerfade', []);
 
-    module.controller('spInnerfade', [
-        '$scope', '$element', '$timeout', '$window',
-        function($scope, $element, $timeout, $window)
+    module.directive('spInnerfade', [
+        '$timeout', '$window',
+        function($timeout, $window)
         {
-            $scope.items = $element[0].querySelectorAll('.sp-innerfade-item');
+            return {
+                restrict: 'AC',
+                link: function ($scope, $element)
+                {
+                    $scope.items = [];
+                    $scope.currentIndex = 0;
 
-            $scope.next = function(){
-                if(this.currentIndex < (this.items.length - 1)){
-                    this.currentIndex++;
-                }else{
-                    this.currentIndex = 0;
+                    $scope.append = function(item) {
+                        $scope.items.push(item);
+                    };
+
+                    $scope.next = function ()
+                    {
+                        if (this.currentIndex < (this.items.length - 1)) {
+                            this.currentIndex++;
+                        } else {
+                            this.currentIndex = 0;
+                        }
+                    };
+
+                    $scope.layout = function ()
+                    {
+                        if (this.items.length >= this.currentIndex + 1) {
+                            $element.css({
+                                height: this.items[this.currentIndex].offsetHeight + 'px'
+                            });
+                        }
+                    };
+
+                    $scope.play = function ()
+                    {
+                        $timeout(function () {
+                            $scope.next();
+                            $scope.play();
+                        }, 5000);
+                    };
+
+                    $scope.$watch('currentIndex', function () {
+                        $scope.layout();
+                    });
+
+                    angular.element($window).bind('resize', function () {
+                        $scope.layout();
+                    });
+
+                    $scope.play();
                 }
             };
-
-            $scope.updateHeight = function(){
-                $element.css({height:$scope.items[this.currentIndex].offsetHeight+'px'});
-            };
-
-            $scope.$watch('currentIndex', function(){
-                $scope.updateHeight();
-            });
-
-            angular.element($window).bind('resize', function(){
-                $scope.updateHeight();
-            });
-
-            $scope.play = function() {
-                $timeout(function() {
-                    $scope.next();
-                    $scope.play();
-                }, 5000);
-            };
-
-            $scope.currentIndex = 0;
-            $scope.play();
         }
     ]);
 
@@ -62,13 +79,16 @@
                 transclude:!0,
                 scope: true,
                 template: '<div ng-show="currentIndex == index" ng-transclude></div>',
-                link: function($scope, $element)
+                link: function(scope, element)
                 {
                     $timeout(function() {
-                        $scope.index = Array.prototype.indexOf.call($scope.items, $element[0]);
+                        var parentScope = element.parent('*[sp-innerfade]:first, .sp-innerfade:first').scope();
+                        parentScope.append(element[0]);
 
-                        $element.find('img').bind('load', function() {
-                            $scope.updateHeight();
+                        scope.index = Array.prototype.indexOf.call(scope.items, element[0]);
+
+                        element.find('img').bind('load', function() {
+                            scope.layout();
                         });
                     });
                 }
